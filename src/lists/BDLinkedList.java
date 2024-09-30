51735340fa9e1a67131b289d46a6f1a0312589c4
HollowKn1ght <volodin10088@gmail.com>
2024-09-30 14:50:12 +0300

    Initial commit

---
+++ src/lists/BDLinkedList.java
@@ -0,0 +1,17 @@
+package lists;
+
+import java.util.LinkedList;
+
+public class BLinkedList {
+    private BNode head;
+    private BNode tail;
+
+    BLinkedList(){
+    }
+
+    
+
+    public void add(BNode node){
+
+    }
+}
